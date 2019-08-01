408d245907b62ed05c4b910058950075c4d670f4
KAWAI Shun <shun@osstech.co.jp>
2019-08-01 13:02:11 +0900

    パスをいじる

--- charaViewer/wsgi.py
+++ charaViewer/wsgi.py
@@ -9,6 +9,16 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+VENV_SITE_DIRECTORY = os.environ.get("VENV_SITE_DIRECTORY", None)
+if VENV_SITE_DIRECTORY is not None:
+    sys.path.append(VENV_SITE_DIRECTORY)
+
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'charaViewer.settings')
