23a4a9feb730dbc44b9469d15699c025bb54b452
KAWAI Shun <shun@osstech.co.jp>
2019-08-02 11:15:26 +0900

    エンドポイントの変更

--- charaViewer/urls.py
+++ charaViewer/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 #from django.contrib import admin
-from django.urls import path, include
 import charaViewer.app_chara.urls
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('charaViewer/', include(charaViewer.app_chara.urls))
+    path('', include(charaViewer.app_chara.urls))
 ]
