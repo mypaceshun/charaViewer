9b4c81e198082ec6bed4deb1d64df71280bfb251
KAWAI Shun <shun@osstech.co.jp>
2019-08-02 11:26:25 +0900

    [bug] import文消してた

--- charaViewer/urls.py
+++ charaViewer/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 #from django.contrib import admin
 import charaViewer.app_chara.urls
+from django.urls import path, include
 
 urlpatterns = [
     path('', include(charaViewer.app_chara.urls))
